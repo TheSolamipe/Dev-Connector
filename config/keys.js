88afea57947187a0ee670a8ced5729cf8d8e4ca7
TheSolamipe <akinolasolamipe@gmail.com>
2020-03-18 23:56:47 +0100

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:asd123@cluster0-xp5uy.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+/*importing all dependencies; express for routing,
+mongoose for connecting to and interacting with mongoDB
+passport for authentication*/
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+//CONNECT TO MONGODB
+mongoose
+  .connect(db)
+  .then(() => console.log("db connected successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, i'm working");
+});
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server is listening to port ${port}`));
