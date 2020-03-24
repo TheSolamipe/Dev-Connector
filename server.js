941738957a99119fadc3b5494fdac0b3c40e904b
TheSolamipe <akinolasolamipe@gmail.com>
2020-03-24 15:17:35 +0100

    Created User Model

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ mongoose for connecting to and interacting with mongoDB
 passport for authentication*/
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//loading routes
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB CONFIG
 const db = require("./config/keys").mongoURI;
