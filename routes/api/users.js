6f0a00a7d2b9464fd24b079202d9a746f1ac59a0
TheSolamipe <akinolasolamipe@gmail.com>
2020-04-02 18:34:21 +0100

    Created validation rules for registering

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //loading User Model
 const User = require("../../models/User");
 
@@ -18,9 +21,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users file works" }));
 // @desc:   Register users
 // @access: Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ msg: "User already exists" });
+      errors.email = "User already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
