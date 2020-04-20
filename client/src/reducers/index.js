a20a0ee496ce01e6fa83a0e96a490a91313e5b7c
TheSolamipe <akinolasolamipe@gmail.com>
2020-04-20 13:17:59 +0100

    Created the registration error reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 //this is our root reducer
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
   auth: authReducer,
+  errors: errorReducer,
 });
