f75d1ae984c5bf6fcb5e0015c9a24f48d25955de
TheSolamipe <akinolasolamipe@gmail.com>
2020-04-20 22:38:50 +0100

    Created redux Login and set current user actions,set auth token and created reducer case for current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,15 +1,52 @@
-import { GET_ERRORS } from "./types";
+import React from "react";
 import axios from "axios";
+import Loading from "../components/layout/Loading";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => (dispatch) => {
   axios
     .post(`${process.env.REACT_APP_BASE_URL}/api/users/register`, userData)
-    .then((res) => history.push("/login"))
+    .then((res) => history.push("/login") || <Loading />)
     .catch((err) =>
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data,
       })
     );
+};
+
+//Login - Get User Token
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post(`${process.env.REACT_APP_BASE_URL}/api/users/login`, userData)
+    .then((res) => {
+      //Save to Local Storage
+      const { token } = res.data;
+      //Set token to local Storage
+      localStorage.setItem("jwtToken", token);
+      //Set token to AuthHeader
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+//Set logged in User
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
 };
---
+++ client/src/components/layout/Loading.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Bone } from "react-loading-skeleton-placeholders";
+
+const Loading = () => {
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">
+              <Bone width={10} />
+            </h1>
+            <p className="lead text-center">
+              <Bone />
+            </p>
+            <form>
+              <div className="form-group">
+                <Bone height={25} />
+              </div>
+              <div className="form-group">
+                <Bone height={25} />
+              </div>
+              <Bone />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
