26770f410d89da5d9195ee916e99a556d61eae29
TheSolamipe <akinolasolamipe@gmail.com>
2020-04-28 00:04:55 +0100

    Created Dashboard Education & Experience displays with delete actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -66,6 +66,42 @@ export const addEducation = (expData, history) => (dispatch) => {
     );
 };
 
+//Delete Experience
+export const deleteExperience = (id) => (dispatch) => {
+  axios
+    .delete(`${process.env.REACT_APP_BASE_URL}/api/profile/experience/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+//Delete Education
+export const deleteEducation = (id) => (dispatch) => {
+  axios
+    .delete(`${process.env.REACT_APP_BASE_URL}/api/profile/education/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
 //Delete Account & profile
 export const deleteAccount = () => (dispatch) => {
   if (window.confirm("Are you sure? This CAN NOT be undone!")) {
