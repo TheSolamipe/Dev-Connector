c07c7c7f87c5b91d3ab10a2966230c440cb923be
TheSolamipe <akinolasolamipe@gmail.com>
2020-04-29 02:25:17 +0100

    Added Profile About, Credentials and Github components

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -96,6 +97,7 @@ class App extends React.Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,37 +1,37 @@
 import React, { Component } from "react";
+import isEmpty from "../../validation/is-empty";
+import PropTypes from "prop-types";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    //Get FirstName
+    const firstName = profile.user.name.trim().split(" ")[0];
+    //Skills List
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">John's Bio</h3>
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
             <p className="lead">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Provident fuga cum necessitatibus blanditiis vel, officia facere
-              porro esse numquam assumenda doloremque saepe aliquam nemo
-              excepturi aliquid maiores! Excepturi, libero repudiandae.
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} does not have a bio</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
             </p>
             <hr />
             <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
               <div className="d-flex flex-wrap justify-content-center align-items-center">
-                <div className="p-3">
-                  <i className="fa fa-check"></i> HTML
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check"></i> CSS
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check"></i> JavaScript
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check"></i> Python
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check"></i> C#
-                </div>
+                {skills}
               </div>
             </div>
           </div>
@@ -40,5 +40,9 @@ class ProfileAbout extends Component {
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default ProfileAbout;
